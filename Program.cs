2870a876e39db1c15b2d4a9a6a12c10cd5113a02
maksim kireev <maksimkir987@gmail.com>
2024-03-07 21:10:25 +0300

    create method FilterStrings

--- Program.cs
+++ Program.cs
@@ -7,3 +7,35 @@ for (int i = 0; i < n; i++)
 {
     array[i] = Console.ReadLine();
 }
+
+string[] newarray = FilterStrings(array);
+
+Console.WriteLine("\nНовый массив строк, длина которых меньше или равна 3 символам:");
+for (int i = 0; i < newarray.Length; i++)
+{
+    Console.WriteLine(newarray[i]);
+}
+
+static string[] FilterStrings(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+string[] newarray = new string[count];
+int index = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newarray[index] = array[i];
+        index++;
+    }
+}
+return newarray;
+}
